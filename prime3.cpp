94746b12556663a833ec9e78d48c62763d1b1a49
keerthi2902 <84083884+keerthi2902@users.noreply.github.com>
2021-05-12 19:29:11 +0530

    Add files via upload

---
+++ prime3.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int i,num,count=0;
+    cout<<"Enter a number" ;
+    cin>>num;
+    for(i=2;i<=sqrt(num+1);i++)
+    {
+      if(num%i==0)
+      {
+        count++;
+        cout<< "not a whole number";
+        
+       }
+    }
+    if(count==0) 
+       cout<<" a whole number";
+    
+ }
+ 
